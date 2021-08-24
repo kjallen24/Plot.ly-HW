a79af5cdaa4fb42adac8c443a9a66e2d97615f46
kallen <kadatagirl@gmail.com>
2021-08-24 18:13:54 -0500

    hw

---
+++ StarterCode/static/js/app.js
@@ -0,0 +1,28 @@
+//Use D3 Library to read in samples.json
+
+
+//Create Horizontal BarChart w/ drp menu
+//// use sample_values as barchart values, otu_ids as labels for barchart and otu_labels as hovertext for barchart
+
+//Create bubble chat that displays: Use otu_ids for the x values.
+function createchart(names1){
+  
+
+
+}
+
+
+function initialize(){
+  var celldropdown = d3.select("#selDataset");
+  d3.json("samples.json").then((data)=>{
+
+    var samplesnames = data.names;
+    samplesnames.forEach((sample)=>{
+      celldropdown.append("option").text(sample).property("value",sample);
+    });
+      
+  });
+  var sample1 = samplesnames[0];
+  createchart(sample1);
+}
+initialize();
