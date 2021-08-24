51532f6e21db20604d29a4acbcc09c42e85436d6
kallen <kadatagirl@gmail.com>
2021-08-24 18:42:11 -0500

    hw

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -9,11 +9,41 @@ function createchart(dValue){
   d3.json("samples.json").then((data)=>{
     var mysamples = data.samples;
     var resultarray = mysamples.filter( myobjects=> myobjects.id == dValue);
-    alert(resultarray);
-    console.log(resultarray);
+    var myresult = resultarray[0];
+    var otu_ids = myresult.otu_ids;
+    var otu_labels = myresult.otu_labels;
+    var sample_values= myresult.sample_values;
+
+    var mylayout = {
+      title: "BellyButton Bubble Chart",
+      margin: {t:0},
+      hovermode: "closest",
+      xaxis: {title: "otu_ids"},
+      margin: {t:30}
+    };
+
+    var mydata = [
+      {
+        x: otu_ids,
+        y: sample_values,
+        mode: "markers",
+        marker: {
+          size: sample_values,
+          color: otu_ids,
+          colorscale: "earth",
+
+        }
+      }
+    ];
+
+    Plotly.newPlot("bubble",mydata,mylayout);
   });
 
 
+}
+
+function optionChanged(dValue){
+  createchart(dValue);
 }
 
 
